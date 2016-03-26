9277a167ed2fe0f84cdfe2724a74875126f83825
Diego Cevallos Porta <Weniver@Diegos-MacBook-Pro.local>
2016-03-25 18:57:55 -0600

    Migración de carpeta
    Agrego clase warrior, aplicación, y view ‘select team’

---
+++ app.rb
@@ -0,0 +1,127 @@
+require 'sinatra'
+require './lib/warrior.rb'
+
+enable :sessions
+
+get '/' do
+	"<a href='/play'>PLAY</a>"
+end
+
+get '/play' do
+	"<p><a href='/teams/1'>1 Player</a></p>
+	 <p><a href='/teams/2'>2 Players</a></p>"
+end
+
+get "/teams/:qty" do |qty|
+	players = []
+	(1..qty.to_i).each do |player|
+		players << erb(:team_select, layout: nil, locals: {
+			player: player
+		})
+	end
+
+	<<-HTML
+	<form action="/play/#{qty}" method="post" id="player1">
+	  <h1>Select your team:</h1>
+	 	#{players.join('')}
+		<input type="submit" value="FIGHT">
+	</form>
+	HTML
+end
+
+
+post "/play/:qty" do |qty|
+	parties = []
+	session[:qty] = qty.to_i
+	params[:player].each do |id, players|
+		parties << players.map { |player|
+			Warrior.send(player.to_sym)
+		}
+	end
+
+	if qty.to_i == 1
+		parties[1] = []
+		5.times do 
+			parties[1] << Warrior.send(['normal','strong','fast'].sample)
+		end
+	end
+	session[:parties] = parties
+	redirect to ("/fight/0")	
+end
+
+
+get "/fight/:attacker" do |attacker|
+	enemy = attacker.to_i == 0 ? 1 : 0;
+	turn = attacker.to_i + 1
+	enemigos = session[:parties][enemy].each_with_index.map { |w, index|
+			health = w.health
+			name = w.name
+		%{<input type="radio" name="warrior" value="#{index}" />#{name} Warrior HP: #{health}<br/>}
+	}
+	
+	atacantes = session[:parties][attacker.to_i].each.map { |w|
+			health = w.health
+			name = w.name
+			attack = w.strength
+		%{<li>#{name} Warrior: #{health}hp -- #{attack} attack.</li>}
+	}
+
+	<<-HTML
+	<h1><u>Player #{turn} turn</u></h1>
+	<h3>Attacking team order:</h3>
+	<ol>
+		#{atacantes.join('')}
+	</ol>
+	<h3>Enemy team:</h3>
+	<form method="post" action="/attack/#{attacker}/#{enemy}">
+  	#{enemigos.join('')}
+  	<button type="submit">Attack!</button>
+	</form>
+	HTML
+end
+
+
+post "/attack/:attacker/:enemy" do |attacker, enemy|
+
+	atacante =  session[:parties][attacker.to_i].first
+	enemigo = session[:parties][enemy.to_i][params[:warrior].to_i]
+
+	atacante.attack(enemigo)
+	if enemigo.dead?
+		session[:parties][enemy.to_i].delete(enemigo)
+	end
+
+	first_to_last = session[:parties][attacker.to_i].shift
+	session[:parties][attacker.to_i] << first_to_last
+
+	if session[:parties][enemy.to_i].empty?
+		winner = enemy.to_i == 0 ? 2 : 1;
+		redirect to "victory/#{'player '+winner.to_s}"
+
+	elsif session[:qty] == 1
+			comp_attacker = session[:parties][1].first
+			comp_victim = session[:parties][0].sample
+			comp_attacker.attack(comp_victim)
+
+			first_to_last = session[:parties][1].shift
+			session[:parties][1] << first_to_last
+
+			if comp_victim.dead?
+				session[:parties][0].delete(comp_victim)
+			end
+
+			if session[:parties][0].empty?
+				redirect to "victory/computer"
+			end
+			redirect to "/fight/0"
+	end
+
+	redirect to "/fight/#{enemy}"
+end
+
+
+get "/victory/:player" do
+	winner = params[:player].capitalize
+	"<h1>#{winner} wins!</h1>
+	<p><a href='/play'>Rematch?</a></p>"
+end
---
+++ lib/warrior.rb
@@ -0,0 +1,70 @@
+class Warrior
+	@@basehealth = 1000
+	@@basestrength = 300
+
+	attr_accessor :health, :strength, :name
+	@@phrases = ['DIE!',
+		    			 'TAKE THIS!',
+		   				 'Sorry...NOT!',
+						   'CHIN CHON LING LONG WANCHINKTON!',
+						   'Walk on home boy.'
+	]
+
+	def initialize (name, health=@@basehealth, strength=@@basestrength)
+		@name = name
+		@health = health
+		@strength = strength	
+	end
+	#clases de warrior
+	def self.normal 
+		health = @@basehealth + (100 + rand(0..100))
+		strength = @@basestrength + (15 + rand(0..15))
+		Warrior.new('Normal', health, strength)
+	end
+
+	def self.fast #menos vida mas ataque
+		health = @@basehealth - (200 + rand(0..200))
+		strength = @@basestrength + (30 + rand(0..30))
+		Warrior.new('Fast', health, strength)
+	end
+
+	def self.strong #menos vida mas ataque
+		health = @@basehealth + (200 + rand(0..200))
+		strength = @@basestrength - (30 + rand(0..30))
+		Warrior.new('Strong', health, strength)
+	end
+
+	def attack(enemy)
+		bonus = critical
+		damage = @strength * bonus
+		enemy.health -= damage
+		critical_shout = 'Critical Attack!<br>' if bonus == 2
+		critical_shout = 'Super Critical Attack!<br>' if bonus == 3
+		critical_shout = '' if bonus == 1
+
+		return @@phrases.sample + "<br>#{critical_shout}The attack deals #{damage} damage!"
+	end
+
+	def critical
+		luck = rand(1..100)
+		case luck
+		when 1
+			3
+		when 2..20
+			2
+		else
+			1
+		end
+	end
+
+	def talk(words)
+		puts words
+	end
+
+	def dead?
+		if self.health <= 0
+			return true
+		end
+	end
+
+end
